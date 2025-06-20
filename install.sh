9a67ba478e18dd032712651bae64bf2aeb8d2123
Maksym Dukhota <37228674+themngn@users.noreply.github.com>
2025-06-20 22:00:25 +0300

    Update

--- install.sh
+++ install.sh
@@ -5,11 +5,11 @@ echo "Installing Staff"
 sudo dnf install stow zsh hyprland -y
 sudo dnf install alacritty -y
 sudo dnf isntall waybar -y
-sudo dnf install cmake meson cpio -y #Hyprpm deps
+sudo dnf install hyprland-devel cmake meson cpio -y #Hyprpm deps
 echo "Done"
 
 echo "Installing hyprland plugins"
-hyprpm add https://github.com/Duckonaut/split-monitor-workspaces
+hyprpm add https://github.com/Duckonaut/split-monitor-workspaces -y
 hyprpm enable split-monitor-workspaces
 
 echo "Done"
