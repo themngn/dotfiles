6e870f5c87cfecea379ed6386456514289e1cf2c
Maksym Dukhota <37228674+themngn@users.noreply.github.com>
2025-06-20 21:54:38 +0300

    Update

--- install.sh
+++ install.sh
@@ -6,6 +6,10 @@ sudo dnf install stow zsh hyprland -y
 sudo dnf install alacritty -y
 sudo dnf isntall waybar -y
 
+echo "Installing hyprland plugins"
+hyprpm add https://github.com/Duckonaut/split-monitor-workspaces
+hyprpm enable split-monitor-workspaces
+
 echo "Creating symlinks for dotfiles"
 stow nvim
 stow hyprland
