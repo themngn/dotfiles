8174ef921265f238ed465e7868210848ef35241b
Maksym Dukhota <37228674+themngn@users.noreply.github.com>
2025-06-20 22:54:37 +0300

    Update

--- install.sh
+++ install.sh
@@ -9,12 +9,6 @@ sudo dnf isntall waybar -y
 sudo dnf install hyprland-devel cmake meson cpio -y #Hyprpm deps
 echo "Done"
 
-echo "Installing hyprland plugins"
-hyprpm add https://github.com/Duckonaut/split-monitor-workspaces -y
-hyprpm enable split-monitor-workspaces
-
-echo "Done"
-
 echo "Creating symlinks for dotfiles"
 stow nvim
 stow hyprland
