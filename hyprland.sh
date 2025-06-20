e0244df78ea2c477aaaf4401c7d74dcca29a717d
Maksym Dukhota <37228674+themngn@users.noreply.github.com>
2025-06-20 22:54:41 +0300

    Update

---
+++ hyprland.sh
@@ -0,0 +1,7 @@
+echo "Installing hyprland plugins"
+hyprpm add https://github.com/Duckonaut/split-monitor-workspaces
+hyprpm enable split-monitor-workspaces
+hyprpm add https://github.com/KZDKM/Hyprspace
+hyprpm enable Hyprspace
+hyprpm reload
+echo "Done"
