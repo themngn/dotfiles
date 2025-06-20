834415658902da8af145494b32fa4036dbaf3c4c
Maksym Dukhota <37228674+themngn@users.noreply.github.com>
2025-06-20 21:51:27 +0300

    Update

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@ echo "Welcome!"
 
 echo "Installing Staff"
 sudo dnf install stow zsh hyprland -y
+sudo dnf install alacritty -y
+sudo dnf isntall waybar -y
 
 echo "Creating symlinks for dotfiles"
 stow nvim
