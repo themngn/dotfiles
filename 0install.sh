5b1058de6f9d073459b69d38d97460cb6ce19f2e
Maksym Dukhota <37228674+themngn@users.noreply.github.com>
2025-06-23 19:10:11 +0300

    Update

--- 0install.sh
+++ 0install.sh
@@ -3,8 +3,12 @@ echo "Welcome!"
 
 echo "Installing Staff"
 sudo dnf copr enable solopasha/hyprland -y
-sudo dnf install stow zsh hyprland -y
+sudo dnf install stow -y 
+sudo dnf install hyprland -y
+sudo dnf install zsh -y
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 sudo dnf install alacritty -y
+sudo dnf install rofi -y
 sudo dnf install waybar -y
 sudo dnf install hyprland-devel cmake meson cpio -y #Hyprpm deps
 echo "Done"
@@ -13,15 +17,9 @@ echo "Creating symlinks for dotfiles"
 stow nvim
 stow hyprland
 stow zsh
+stow tmux
 echo "Done"
 
 echo "Settings"
 chsh -s $(which zsh) mono
 echo "Done"
-
-
-
-
-
-
-
